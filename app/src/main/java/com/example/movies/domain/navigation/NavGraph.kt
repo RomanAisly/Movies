fb989f8e8fcf0bd8b3c060478ecbb1e2aad8047d
RomanAisly <romanalexaisly@gmail.com>
2024-04-20 12:46:26 +0300

    still problem

--- app/src/main/java/com/example/movies/domain/navigation/NavGraph.kt
+++ app/src/main/java/com/example/movies/domain/navigation/NavGraph.kt
@@ -7,13 +7,15 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.movies.HomeScreenContent
+import com.example.movies.data.remote.ResultDTO
 import com.example.movies.ui.screens.FavoriteScreen
 import com.example.movies.ui.screens.Search
 import com.example.movies.ui.screens.SettingsScreen
 import com.example.movies.ui.screens.WatchLaterScreen
 
 @Composable
-fun NavGraph() {
+fun NavGraph(films: ResultDTO) {
     val navController = rememberNavController()
     Scaffold(
         bottomBar = { BottomNavBar(navController = navController) },
@@ -23,7 +25,7 @@ fun NavGraph() {
             startDestination = BottomItems.HomeBottomNav.route,
             modifier = Modifier.padding(paddingValues)
         ) {
-            composable(route = BottomItems.HomeBottomNav.route) { }
+            composable(route = BottomItems.HomeBottomNav.route) { HomeScreenContent(films) }
             composable(route = BottomItems.FavoriteBottomNav.route) { FavoriteScreen() }
             composable(route = BottomItems.WatchLaterBottomNav.route) { WatchLaterScreen() }
             composable(route = BottomItems.SettingsBottomNav.route) { SettingsScreen() }
